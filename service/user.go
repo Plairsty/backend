a9baca924c031bc9bd4b533656770e96d558496e
YadavGulshan <yadavgulshan542@gmail.com>
2022-09-15 13:14:11 +0530

    added func for creating user and for seeding user

--- service/user.go
+++ service/user.go
@@ -1,6 +1,10 @@
 package service
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	Username       string
@@ -24,7 +28,7 @@ func NewUser(username, password, role string) (*User, error) {
 // If error is nil, then the password is correct.
 func (user *User) IsCorrectPassword(password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password))
-    return err == nil
+	return err == nil
 }
 
 func (user *User) Clone() *User {
@@ -33,4 +37,28 @@ func (user *User) Clone() *User {
 		HashedPassword: user.HashedPassword,
 		Role:           user.Role,
 	}
+}
+
+func createUser(userStore UserStore, username, password, role string) error {
+
+	user, err := NewUser(username, password, role)
+	if err != nil {
+		return err
+	}
+
+	return userStore.Save(user)
+}
+
+func seedUsers(userStore UserStore) error {
+	err := createUser(userStore, "admin1", "secret", "admin")
+	if err != nil {
+		return err
+	}
+	log.Println("Admin user created")
+	err = createUser(userStore, "user1", "secret", "user")
+	if err != nil {
+		return err
+	}
+	log.Println("User created")
+	return nil
 }
