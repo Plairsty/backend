eef070341debfde9d7e94cbb2e00f3de1ef77626
YadavGulshan <yadavgulshan542@gmail.com>
2022-09-15 20:41:53 +0530

    Added implementation to return a refresh token

--- service/jwt_manager.go
+++ service/jwt_manager.go
@@ -8,8 +8,9 @@ import (
 )
 
 type JWTManager struct {
-	secretKey     string
-	tokenDuration time.Duration
+	secretKey            string
+	tokenDuration        time.Duration
+	refreshTokenDuration time.Duration
 }
 
 type UserClaims struct {
@@ -18,24 +19,48 @@ type UserClaims struct {
 	Role     string `json:"role"`
 }
 
-func NewJWTManager(secretKey string, tokenDuration time.Duration) *JWTManager {
+type JWTToken struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+func NewJWTManager(secretKey string, tokenDuration, refreshTokenDuration time.Duration) *JWTManager {
 	return &JWTManager{
-		secretKey:     secretKey,
-		tokenDuration: tokenDuration,
+		secretKey:            secretKey,
+		tokenDuration:        tokenDuration,
+		refreshTokenDuration: refreshTokenDuration,
 	}
 }
 
-func (manager *JWTManager) Generate(user *User) (string, error) {
-	claims := UserClaims{
+func userClaimProvider(username, role string, expiresAt time.Duration) UserClaims {
+	return UserClaims{
 		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: time.Now().Add(manager.tokenDuration).Unix(),
+			ExpiresAt: time.Now().Add(expiresAt).Unix(),
+			Issuer:    "plairsty",
 		},
-		Username: user.Username,
-		Role:     user.Role,
+		Username: username,
+		Role:     role,
 	}
+}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(manager.secretKey))
+func (manager *JWTManager) Generate(user *User) (JWTToken, error) {
+	claims := userClaimProvider(user.Username, user.Role, manager.tokenDuration)
+	refreshClaims := userClaimProvider(user.Username, user.Role, manager.refreshTokenDuration)
+
+	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	accessTokenVal, err := accessToken.SignedString([]byte(manager.secretKey))
+	if err != nil {
+		return JWTToken{}, err
+	}
+	refreshTokenVal, err := refreshToken.SignedString([]byte(manager.secretKey))
+	if err != nil {
+		return JWTToken{}, err
+	}
+	return JWTToken{
+		AccessToken:  accessTokenVal,
+		RefreshToken: refreshTokenVal,
+	}, nil
 }
 
 func (manager *JWTManager) Verify(accessToken string) (*UserClaims, error) {
@@ -43,7 +68,7 @@ func (manager *JWTManager) Verify(accessToken string) (*UserClaims, error) {
 		Pass in the access token, an empty user claims, and a custom key function.
 		In this function,
 		It’s very important to check the signing method of the token to make sure that
-		it matches with the algorithm our server uses, which in our case is RSA.
+		it matches with the algorithm our server uses, which in our case is RSA. (Now HMAC 🥹)
 		If it matches, then we just return the secret key that is used to sign the token.
 	*/
 
@@ -51,7 +76,7 @@ func (manager *JWTManager) Verify(accessToken string) (*UserClaims, error) {
 		accessToken,
 		&UserClaims{},
 		func(token *jwt.Token) (interface{}, error) {
-			_, ok := token.Method.(*jwt.SigningMethodRSA)
+			_, ok := token.Method.(*jwt.SigningMethodHMAC)
 			if !ok {
 				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 			}
