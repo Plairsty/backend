cd9cd07e112841c0908663df67142fa8998958c3
YadavGulshan <yadavgulshan542@gmail.com>
2022-09-15 23:35:10 +0530

    Added interceptors that will attach tokens to all the rpc calls that needs authorization

---
+++ cmd/client/service/interceptor.go
@@ -0,0 +1,57 @@
+package service
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"log"
+)
+
+// interceptors to attach the token to the request context.
+
+// Unary Inside this interceptor function, let’s write a simple log with the calling method name.
+// Then check if this method needs authentication or not. If the method doesn’t require authentication,
+// then nothing to be done, we simply invoke the RPC with the original context.
+//
+// If it does, we must attach the access token to the context before invoking the actual RPC.
+func (interceptor *AuthInterceptor) Unary() grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req, reply interface{},
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		log.Printf("--> unary interceptor: %s", method)
+
+		if interceptor.authMethod[method] {
+			return invoker(interceptor.attachToken(ctx), method, req, reply, cc, opts...)
+		}
+
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
+func (interceptor *AuthInterceptor) attachToken(ctx context.Context) context.Context {
+	return metadata.AppendToOutgoingContext(ctx, "authorization", interceptor.accessToken)
+}
+
+func (interceptor *AuthInterceptor) Stream() grpc.StreamClientInterceptor {
+	return func(
+		ctx context.Context,
+		desc *grpc.StreamDesc,
+		cc *grpc.ClientConn,
+		method string,
+		streamer grpc.Streamer,
+		opts ...grpc.CallOption,
+	) (grpc.ClientStream, error) {
+		log.Printf("--> stream interceptor: %s", method)
+
+		if interceptor.authMethod[method] {
+			return streamer(interceptor.attachToken(ctx), desc, cc, method, opts...)
+		}
+
+		return streamer(ctx, desc, cc, method, opts...)
+	}
+}
