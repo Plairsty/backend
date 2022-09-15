dd69e26c04175b1391a68fbdb09e54c56e4b7032
YadavGulshan <yadavgulshan542@gmail.com>
2022-09-15 13:04:57 +0530

    Added jwt manager for to create, generate, and verify the tokens

--- service/jwt_manager.go
+++ service/jwt_manager.go
@@ -1,8 +1,70 @@
 package service
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
 
 type JWTManager struct {
 	secretKey     string
 	tokenDuration time.Duration
+}
+
+type UserClaims struct {
+	jwt.StandardClaims
+	Username string `json:"username"`
+	Role     string `json:"role"`
+}
+
+func NewJWTManager(secretKey string, tokenDuration time.Duration) *JWTManager {
+	return &JWTManager{
+		secretKey:     secretKey,
+		tokenDuration: tokenDuration,
+	}
+}
+
+func (manager *JWTManager) Generate(user *User) (string, error) {
+	claims := UserClaims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(manager.tokenDuration).Unix(),
+		},
+		Username: user.Username,
+		Role:     user.Role,
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(manager.secretKey))
+}
+
+func (manager *JWTManager) Verify(accessToken string) (*UserClaims, error) {
+	/*
+		Pass in the access token, an empty user claims, and a custom key function.
+		In this function,
+		It’s very important to check the signing method of the token to make sure that
+		it matches with the algorithm our server uses, which in our case is RSA.
+		If it matches, then we just return the secret key that is used to sign the token.
+	*/
+
+	token, err := jwt.ParseWithClaims(
+		accessToken,
+		&UserClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			_, ok := token.Method.(*jwt.SigningMethodRSA)
+			if !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return []byte(manager.secretKey), nil
+		},
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("invalid token: %w", err)
+	}
+	claims, ok := token.Claims.(*UserClaims)
+	if !ok {
+		return nil, fmt.Errorf("invalid token claims")
+	}
+	return claims, nil
 }
