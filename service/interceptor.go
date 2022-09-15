3489cb340e60417ea2b0b25ae1c7295477e76fe6
YadavGulshan <yadavgulshan542@gmail.com>
2022-09-15 23:36:52 +0530

    Called Authorize function in order to check if the rpc call required authorization or not. If yes, then look for JWT Access token.

--- service/interceptor.go
+++ service/interceptor.go
@@ -7,24 +7,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-func UnaryInterceptor(ctx context.Context,
-	req interface{},
-	info *grpc.UnaryServerInfo,
-	handler grpc.UnaryHandler,
-) (interface{}, error) {
-	log.Println("--> Unary Interceptor", info.FullMethod)
-	return handler(ctx, req)
-}
-
-func StreamInterceptor(srv interface{},
-	ss grpc.ServerStream,
-	info *grpc.StreamServerInfo,
-	handler grpc.StreamHandler,
-) error {
-	log.Println("--> Stream Interceptor", info.FullMethod)
-	return handler(srv, ss)
-}
-
 func (interceptor *AuthInterceptor) Unary() grpc.UnaryServerInterceptor {
 	return func(
 		ctx context.Context,
@@ -33,6 +15,10 @@ func (interceptor *AuthInterceptor) Unary() grpc.UnaryServerInterceptor {
 		handler grpc.UnaryHandler,
 	) (resp interface{}, err error) {
 		log.Println("--> Unary Auth Interceptor", info.FullMethod)
+		err = interceptor.Authorize(ctx, info.FullMethod)
+		if err != nil {
+			return nil, err
+		}
 		return handler(ctx, req)
 	}
 }
@@ -45,6 +31,10 @@ func (interceptor *AuthInterceptor) Stream() grpc.StreamServerInterceptor {
 		handler grpc.StreamHandler,
 	) error {
 		log.Println("--> Stream Auth Interceptor", info.FullMethod)
+		err := interceptor.Authorize(ss.Context(), info.FullMethod)
+		if err != nil {
+			return err
+		}
 		return handler(srv, ss)
 	}
 }
