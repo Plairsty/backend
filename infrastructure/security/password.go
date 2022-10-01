a1798f1046ccafeff5efaea03ae5a5799f2b3e98
YadavGulshan <yadavgulshan542@gmail.com>
2022-10-01 12:25:22 +0530

    Moved files to infrastructure

--- infrastructure/security/password.go
+++ infrastructure/security/password.go
@@ -1,10 +1,53 @@
 package security
 
 import (
-	"crypto/rand"
 	"encoding/base64"
+	"math/rand"
+	"strings"
+	"time"
+
 	"golang.org/x/crypto/bcrypt"
 )
+
+var (
+	lowerCharSet   = "abcdedfghijklmnopqrst"
+	upperCharSet   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialCharSet = "!@#$%&*"
+	numberSet      = "0123456789"
+	allCharSet     = lowerCharSet + upperCharSet + specialCharSet + numberSet
+)
+
+func GeneratePassword() string {
+	rand.Seed(time.Now().Unix())
+	var password strings.Builder
+	for i := 0; i < 8; i++ {
+		random := rand.Intn(len(specialCharSet))
+		password.WriteByte(specialCharSet[random])
+	}
+	for i := 0; i < 8; i++ {
+		random := rand.Intn(len(numberSet))
+		password.WriteByte(numberSet[random])
+	}
+	for i := 0; i < 8; i++ {
+		random := rand.Intn(len(lowerCharSet))
+		password.WriteByte(lowerCharSet[random])
+	}
+
+	for i := 0; i < 8; i++ {
+		random := rand.Intn(len(upperCharSet))
+		password.WriteByte(upperCharSet[random])
+	}
+
+	for i := 0; i < 8; i++ {
+		random := rand.Intn(len(allCharSet))
+		password.WriteByte(allCharSet[random])
+	}
+
+	iRune := []rune(password.String())
+	rand.Shuffle(len(iRune), func(i, j int) { iRune[i], iRune[j] = iRune[j], iRune[i] })
+	return password.String()
+}
+
 
 func Hash(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
